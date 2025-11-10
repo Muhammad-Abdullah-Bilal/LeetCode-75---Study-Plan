9ff2a5f30a763e102d2e17abfbf6e7f236795562
Muhammad-Abdullah-Bilal <abdullahbilal332333@gmail.com>
2025-11-10 19:40:32 +0500

    Added solution of string compression problem

---
+++ String Compression/443.py
@@ -0,0 +1,19 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        w = 0  
+        i = 0  
+        while i < len(chars):
+           char = chars[i]
+           count = 0
+           while i < len(chars) and chars[i] == char:
+              i += 1
+              count += 1
+           chars[w] = char
+           w += 1
+        
+           if count > 1:
+            for c in str(count):
+                chars[w] = c
+                w += 1
+    
+        return w
